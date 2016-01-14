7940e7edf50a588bee0e5270acf30cb3c7ae69a0
yogiraj07 <yogirajawati@gmail.com>
2016-01-13 19:50:02 -0500

    read file

--- MapReduceProgramming/Yogiraj_SequentialAnalysis/src/com/mapreduce/main/SequentialProcess.java
+++ MapReduceProgramming/Yogiraj_SequentialAnalysis/src/com/mapreduce/main/SequentialProcess.java
@@ -1,8 +1,32 @@
 package com.mapreduce.main;
-//
-public class SequentialProcess {
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+
+public class SequentialProcess {
 	public static void main(String[] args) {
+		try {
+			GZIPInputStream inputFile=new GZIPInputStream(new FileInputStream("C:\\Users\\Yogiraj\\Downloads\\323.csv.gz"));
+		    
+			BufferedReader  r = new BufferedReader (new InputStreamReader(inputFile));
+		    String temp = r.readLine();
+		    //String[] data;
+		    int i =1;
+			while(temp !=null)
+		    {
+		    i++;
+		    	temp = r.readLine();
+		    }
+			System.out.println(i);
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
